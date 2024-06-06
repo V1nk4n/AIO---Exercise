d2494153508ab8a33e007368ec60f727f0fe47d0
V1nk4n <vinhan188@gmail.com>
2024-06-06 19:39:49 +0700

    Fix name of file and function

--- module1/week1/main.py
+++ module1/week1/main.py
@@ -1,25 +1,25 @@
-import ex01_metric
-import ex02_activation_func
-import ex03_loss_func
-import module1.week1.ex04_approx as ex04_approx
-import ex05_mean_root_error
+import ex01
+import ex02
+import ex03
+import ex04
+import ex05
 
 if __name__ == "__main__":
 
     print("Exercise 01")
-    ex01_metric.exercise1(tp=2, fp=3, fn=4)
+    ex01.exercise1(tp=2, fp=3, fn=4)
 
     print("Exercise 02")
-    ex02_activation_func.exercise2()
+    ex02.exercise2()
 
     print("Exercise 03")
-    ex03_loss_func.exercise3()
+    ex03.exercise3()
     
     print("Exercise 04")
-    print(ex04_approx.approx_sin(x=3.14, n=10))
-    print(ex04_approx.approx_cos(x=3.14, n=10))
-    print(ex04_approx.approx_sinh(x=3.14, n=10))
-    print(ex04_approx.approx_cosh(x=3.14, n=10))
+    print(ex04.approx_sin(x=3.14, n=10))
+    print(ex04.approx_cos(x=3.14, n=10))
+    print(ex04.approx_sinh(x=3.14, n=10))
+    print(ex04.approx_cosh(x=3.14, n=10))
 
     print("Exercise 05")
-    print(ex05_mean_root_error.md_nre_single_sample(y=100, y_hat=99.5, n=2, p=1))
+    print(ex05.md_nre_single_sample(y=100, y_hat=99.5, n=2, p=1))
